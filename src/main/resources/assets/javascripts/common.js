abe1689464fd6c4660f8d1860b4c1822f63e33cc
HirokiAkaike <you@example.com>
2016-06-12 13:07:50 +0900

    画面要素追加

---
+++ src/main/resources/assets/javascripts/common.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 弘樹 on 2016/06/11.
+ */
+
+function closePopUp() {
+    fukidashi.style.display = "none";
+}
+
+function openPopUp() {
+    closePopUp();
+    fukidashi.style.display = "block";
+}
