2d47629f969d3cc8cae62cec30e790255505d98a
HirokiAkaike <you@example.com>
2016-06-17 17:11:57 +0900

    6/17 push

--- src/main/java/com/nsb/App.java
+++ src/main/java/com/nsb/App.java
@@ -1,12 +1,17 @@
 package com.nsb;
 
+import com.nsb.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
--- src/main/java/com/nsb/web/TopController.java
+++ src/main/java/com/nsb/web/TopController.java
@@ -1,14 +1,20 @@
 package com.nsb.web;
 
+import com.nsb.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class TopController {
+    @Autowired
+    RoomRepository roomRepository;
 
-    @RequestMapping("/")
+    @RequestMapping(method=RequestMethod.GET)
     public String top() {
+        roomRepository.findAll().forEach(System.out::println);
         return "index";
     }
 }
